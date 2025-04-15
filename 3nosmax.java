7e24b04899977cc0e75208af814d36be8359f68f
rishitaaavedwal <rishita.vedwal@s.amity.edu>
2025-04-15 20:44:29 +0530

    max of 3 nos

---
+++ 3nosmax.java
@@ -0,0 +1,27 @@
+import.java.util*;
+class 3nosmax
+{
+	public static void main()
+	{
+		int n1, n2, n3, max=0;
+		Scanner sc- new Scanner (System.in);
+		System.out.println("enter 1st no: ");
+		n1=sc.nextInt();
+		System.out.println("enter 2nd no: ");
+		n2=sc.nextInt();
+		System.out.println("enter 3rd no: ");
+		n3-sc.nextInt();
+		if (n1>n2 && n1>n3)
+		{
+			max=n1;
+		}
+		else if (n2>n1 && n2>n3)
+		{
+			max=n2;
+		}
+		else
+		{
+			max=n3;
+		}
+	}
+}
